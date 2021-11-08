5074a92e2bc192c1dae967e04f5c3f3446692324
trinddade29@gmail.com <trinddade29@gmail.com>
2021-11-08 20:32:47 -0300

    create component ContinentBanner

--- src/pages/continent/[slug].tsx
+++ src/pages/continent/[slug].tsx
@@ -1,4 +1,5 @@
 import { Flex } from "@chakra-ui/react";
+import ContinentBanner from "../../components/ContinentBanner";
 import Header from "../../components/Header";
 
 export default function Continent() {
@@ -6,6 +7,7 @@ export default function Continent() {
     <Flex direction="column">
       <title>Europa</title>
       <Header />
+      <ContinentBanner />
     </Flex>
   );
 }
